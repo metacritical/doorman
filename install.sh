d2e69b1468538867294ff037b23d5ff249a299e4
Pankaj Doharey <pankajdoharey@gmail.com>
2024-05-30 03:27:16 +0530

    Update install.sh
    Update message.

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ REPO="https://github.com/metacritical/doormap"
 LATEST_RELEASE_URL="$REPO/releases/download/v0.0.1/doorman"
 INSTALL_DIR="/usr/local/bin"
 
-echo "Installing killport..."
+echo "Installing doorman..."
 curl -L "$LATEST_RELEASE_URL" -o "${INSTALL_DIR}/doorman"
 chmod +x "${INSTALL_DIR}/doorman"
