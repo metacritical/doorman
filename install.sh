8eeef52bb6fe96203e926aac193aeeb0d2618919
Pankaj Doharey <pankajdoharey@gmail.com>
2024-06-18 18:04:11 +0530

    Update install.sh
    v0.0.2

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 OS=$(uname)
 
 REPO="https://github.com/metacritical/doorman"
-LATEST_RELEASE_URL="$REPO/releases/download/v0.0.1/doorman"
+LATEST_RELEASE_URL="$REPO/releases/download/v0.0.2/doorman"
 INSTALL_DIR="/usr/local/bin"
 
 echo "Installing doorman..."
