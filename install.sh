2b7296b45ea408546e1362ae5ac4ff0620c0957f
Pankaj Doharey <pankajdoharey@gmail.com>
2024-06-20 16:36:04 +0530

    Update install.sh
    Updates install script.

--- install.sh
+++ install.sh
@@ -10,5 +10,4 @@ LATEST_RELEASE_URL="$REPO/releases/download/v0.0.2/doorman"
 INSTALL_DIR="/usr/local/bin"
 
 echo "Installing doorman..."
-curl -L "$LATEST_RELEASE_URL" -o "${INSTALL_DIR}/doorman"
-chmod +x "${INSTALL_DIR}/doorman"
+sudo curl -L "$LATEST_RELEASE_URL" -o "${INSTALL_DIR}/doorman" && chmod +x "${INSTALL_DIR}/doorman"
